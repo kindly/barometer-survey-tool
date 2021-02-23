70df33950e456ab29078aa712be53130cd93b0ce
David Raznick <kindly@gmail.com>
2021-02-23 23:42:21 +0000

    add logging

--- survey/settings.py
+++ survey/settings.py
@@ -174,3 +174,31 @@ AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
 
 SURVEY_QUESTIONS_SHEET=env('SURVEY_QUESTIONS_SHEET')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        '': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+        },
+    },
+}
